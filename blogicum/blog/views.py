15e6916db724fa252f521b4451743b7f7179970b
Давид Арутюнов <david.arutyuno@gmail.com>
2025-03-09 14:54:12 +0300

    add secend commit

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -45,18 +45,15 @@ posts = [
 
 
 def index(request):
-    template = 'blog/index.html'
     context = {'posts': posts}
-    return render(request, template, context)
+    return render(request, 'blog/index.html', context)
 
 
-def post_detail(request, id):
-    template = 'blog/detail.html'
-    context = {'post': posts[id]}
-    return render(request, template, context)
+def post_detail(request, post_id):
+    context = {'post': posts[post_id]}
+    return render(request, 'blog/detail.html', context)
 
 
 def category_posts(request, category_slug):
-    template = 'blog/category.html'
     context = {'category': category_slug}
-    return render(request, template, context)
+    return render(request, 'blog/category.html', context)
--- blogicum/pages/views.py
+++ blogicum/pages/views.py
@@ -1,15 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
-
 
 def about(request):
-    template = 'pages/about.html'
-    context = dict()
-    return render(request, template, context)
+    return render(request, 'pages/about.html')
 
 
 def rules(request):
-    template = 'pages/rules.html'
-    context = dict()
-    return render(request, template, context)
+    return render(request, 'pages/rules.html')
