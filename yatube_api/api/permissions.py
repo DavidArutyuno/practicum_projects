be741260875829a94243601ecebde9e6f7c3ce96
Давид Арутюнов <david.arutyuno@gmail.com>
2025-06-28 18:33:18 +0300

    Add second commit

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -17,9 +17,9 @@ class IsAuthorOrReadOnly(BasePermission):
 
     def has_permission(self, request, view):
         return bool(
-            request.method in SAFE_METHODS or
-            request.user and
-            request.user.is_authenticated
+            request.method in SAFE_METHODS
+            or request.user
+            and request.user.is_authenticated
         )
 
     def has_object_permission(self, request, view, obj):
