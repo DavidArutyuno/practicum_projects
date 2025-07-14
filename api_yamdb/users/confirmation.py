65441e94eee94460b1b5f37593d9a7544fbb8e7e
Давид Арутюнов <david.arutyuno@gmail.com>
2025-07-14 16:42:51 +0300

    Тема письма все равно в base64 приходит.

--- api_yamdb/users/confirmation.py
+++ api_yamdb/users/confirmation.py
@@ -1,7 +1,8 @@
 import secrets
+from email.header import Header
 
 from django.conf import settings
-from django.core.mail import send_mail
+from django.core.mail import EmailMessage
 
 
 def get_confirmation_code(length=32):
@@ -14,11 +15,14 @@ def get_confirmation_code(length=32):
 
 
 def send_confirmation_code(email, confirmation_code):
-    """"Отправка на email кода подтверждения."""
-    send_mail(
-        subject=settings.SUBJECT_EMAIL,
-        message=f'{settings.MESSAGE_EMAIL}: {confirmation_code}',
+    """Отправка на email кода подтверждения."""
+    subject = Header(settings.SUBJECT_EMAIL, 'utf-8').encode()
+    message = f'{settings.MESSAGE_EMAIL}: {confirmation_code}'
+
+    email = EmailMessage(
+        subject=subject,
+        body=message,
         from_email=settings.DEFAULT_FROM_EMAIL,
-        recipient_list=[email],
-        fail_silently=True,
+        to=[email],
     )
+    email.send(fail_silently=True)
