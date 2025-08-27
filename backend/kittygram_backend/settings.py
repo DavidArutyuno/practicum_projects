dca9811838ed4db66271ec93c2d21bbb544b142d
Давид Арутюнов <david.arutyuno@gmail.com>
2025-08-27 15:36:13 +0300

    Add 6 commit

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -1,15 +1,19 @@
 import os
 from pathlib import Path
 
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-cg6*%6d51ef8f#4!r3*$vmxm4)abgjw8mo!4y-q*uq1!4$-89$'
+SECRET_KEY = os.getenv(
+    'SECRET_KEY', default='django-insecure-test-key-for-dev-only-1234567890')
 
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = [
-    'localhost', '89.169.175.171', '127.0.0.1', '173.19.0.3', 'www.kittygram777.ru', 'kittygram777.ru'
-]
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', default='127.0.0.1,localhost').split(',')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -85,7 +89,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-RU'
 
 TIME_ZONE = 'UTC'
 
