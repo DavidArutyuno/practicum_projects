790e932cec2ec2b084c0f68838bad8406708dde4
Давид Арутюнов <david.arutyuno@gmail.com>
2025-10-23 00:47:35 +0300

    add 2.3 version

--- yacut/views.py
+++ yacut/views.py
@@ -1,7 +1,6 @@
 import asyncio
-from http import HTTPStatus
 
-from flask import abort, flash, redirect, render_template
+from flask import flash, redirect, render_template
 
 from . import app, db
 from .forms import GenerateLinkForm, LoadFilesForm
