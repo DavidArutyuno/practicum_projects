1a9250b31c9248eaf99d5b31344b875df0c281c3
Давид Арутюнов <david.arutyuno@gmail.com>
2025-10-23 01:24:00 +0300

    add 2.6 version

--- settings.py
+++ settings.py
@@ -2,9 +2,12 @@ import os
 
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
-    SECRET_KEY = os.getenv('SECRET_KEY')
-    DISK_TOKEN = os.getenv('DISK_TOKEN')
-    FLASK_RUN_PORT = os.getenv('FLASK_RUN_PORT')
-    BASE_URL = os.getenv('BASE_URL')
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        'DATABASE_URI', default='sqlite:///db.sqlite3')
+    SECRET_KEY = os.getenv(
+        'SECRET_KEY', default='SUP3R-S3CR3T-K3Y-F0R-MY-PR0J3C')
+    DISK_TOKEN = os.getenv(
+        'DISK_TOKEN', default='y0_nbfoiu3445tno35_fd09v854bn2_cs0e8hrb4k')
+    FLASK_RUN_PORT = os.getenv('FLASK_RUN_PORT', default=5000)
+    BASE_URL = os.getenv('BASE_URL', default='http://localhost')
     MAX_LENGTH = 16
